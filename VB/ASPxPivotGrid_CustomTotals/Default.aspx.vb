1826482fd477e1171338bace96ffbd4f13eb6875
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/ASPxPivotGrid_CustomTotals/Default.aspx.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+
+Namespace CustomTotals
+	Partial Public Class _Default
+		Inherits System.Web.UI.Page
+		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		End Sub
+	End Class
+End Namespace
