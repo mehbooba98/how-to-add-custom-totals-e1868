0b7b2644d36d6bbf92f078137940a03e482b690e
DevExpressExampleBot <fakeEmail>
2018-04-24 18:19:32 +0300

    Source auto update [skip ci]

--- VB/ASPxPivotGrid_CustomTotals/Default.aspx.vb
+++ VB/ASPxPivotGrid_CustomTotals/Default.aspx.vb
@@ -1,10 +1,10 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 
 Namespace CustomTotals
-	Partial Public Class _Default
-		Inherits System.Web.UI.Page
-		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-		End Sub
-	End Class
+    Partial Public Class _Default
+        Inherits System.Web.UI.Page
+
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        End Sub
+    End Class
 End Namespace
